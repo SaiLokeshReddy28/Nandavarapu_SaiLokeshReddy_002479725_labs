a77bb7f39b87fba3e0f73239d0c9281517c4d939
SaiLokeshReddy28 <nandavarapu.s@northeastern.edu>
2024-10-12 22:24:31 -0400

    classes created

--- Lab_4/src/model/Product.java
+++ Lab_4/src/model/Product.java
@@ -9,5 +9,42 @@ package model;
  * @author sailokeshreddynandavarapu
  */
 public class Product {
+    private String name;
+    private int price;
+    private int id;
+
+    private static int count = 0;
     
+    public Product() {
+        count++;
+        id = count;
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
+    @Override
+    public String toString() {
+        return name;
+    }
 }
